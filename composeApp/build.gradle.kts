1df2a334fe2122e8882fd5f682e584c6a8436efd
Paulo Richa <paulo.richa@hotmail.com>
2026-07-07 22:51:32 -0300

    chore: bump version to 3.0.2 (Build 22)

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -100,8 +100,8 @@ android {
         applicationId = "com.lpstudio.bolaodagalera"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 21
-        versionName = "3.0.1"
+        versionCode = 22
+        versionName = "3.0.2"
     }
     packaging {
         resources {
