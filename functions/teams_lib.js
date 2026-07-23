2ad5f5672666426ebd962409f2bfdcfd43d3f61f
Paulo Richa <paulo.richa@hotmail.com>
2026-07-23 18:04:34 -0300

    Fix: Update Libertadores team IDs for API v4 and adjust UI padding for crests

--- functions/teams_lib.js
+++ functions/teams_lib.js
@@ -1,32 +1,36 @@
 const LIB_TEAMS = {
-    "River Plate": { name: "River Plate", flag: "", code: "RIV", crest: "https://crests.football-data.org/1759.png" },
-    "Club Atlético River Plate": { name: "River Plate", flag: "", code: "RIV", crest: "https://crests.football-data.org/1759.png" },
+    // ARGENTINA
+    "River Plate": { name: "River Plate", flag: "", code: "RIV", crest: "https://crests.football-data.org/1106.png" },
+    "Club Atlético River Plate": { name: "River Plate", flag: "", code: "RIV", crest: "https://crests.football-data.org/1106.png" },
+    "Boca Juniors": { name: "Boca Juniors", flag: "", code: "BOC", crest: "https://crests.football-data.org/1105.png" },
+    "Club Atlético Boca Juniors": { name: "Boca Juniors", flag: "", code: "BOC", crest: "https://crests.football-data.org/1105.png" },
+    "CA Talleres": { name: "Talleres", flag: "", code: "TAL", crest: "https://crests.football-data.org/1104.png" },
+    "Club Atlético Talleres": { name: "Talleres", flag: "", code: "TAL", crest: "https://crests.football-data.org/1104.png" },
+    "Estudiantes": { name: "Estudiantes", flag: "", code: "EST", crest: "https://crests.football-data.org/1108.png" },
+    "Club Estudiantes de La Plata": { name: "Estudiantes", flag: "", code: "EST", crest: "https://crests.football-data.org/1108.png" },
 
+    // URUGUAI
     "Peñarol": { name: "Peñarol", flag: "", code: "PEN", crest: "https://crests.football-data.org/1805.png" },
     "Club Atlético Peñarol": { name: "Peñarol", flag: "", code: "PEN", crest: "https://crests.football-data.org/1805.png" },
-
     "Nacional": { name: "Nacional", flag: "", code: "NAC", crest: "https://crests.football-data.org/1802.png" },
     "Club Nacional de Football": { name: "Nacional", flag: "", code: "NAC", crest: "https://crests.football-data.org/1802.png" },
 
+    // CHILE / PARAGUAI / EQUADOR
     "Colo Colo": { name: "Colo-Colo", flag: "", code: "COL", crest: "https://crests.football-data.org/1792.png" },
     "Club Social y Deportivo Colo-Colo": { name: "Colo-Colo", flag: "", code: "COL", crest: "https://crests.football-data.org/1792.png" },
-
     "Cerro Porteño": { name: "Cerro Porteño", flag: "", code: "CCP", crest: "https://crests.football-data.org/1812.png" },
     "Club Cerro Porteño": { name: "Cerro Porteño", flag: "", code: "CCP", crest: "https://crests.football-data.org/1812.png" },
-
     "LDU": { name: "LDU", flag: "", code: "LDU", crest: "https://crests.football-data.org/1797.png" },
     "LDU de Quito": { name: "LDU", flag: "", code: "LDU", crest: "https://crests.football-data.org/1797.png" },
     "Liga Deportiva Universitaria": { name: "LDU", flag: "", code: "LDU", crest: "https://crests.football-data.org/1797.png" },
-
     "Independiente del Valle": { name: "Ind. del Valle", flag: "", code: "IDV", crest: "https://crests.football-data.org/1796.png" },
     "Club de Alto Rendimiento Especializado Independiente del Valle": { name: "Ind. del Valle", flag: "", code: "IDV", crest: "https://crests.football-data.org/1796.png" },
 
-    "CA Talleres": { name: "Talleres", flag: "", code: "TAL", crest: "https://crests.football-data.org/1781.png" },
-    "Club Atlético Talleres": { name: "Talleres", flag: "", code: "TAL", crest: "https://crests.football-data.org/1781.png" },
-
+    // COLÔMBIA
     "Junior FC": { name: "Junior", flag: "", code: "JUN", crest: "https://crests.football-data.org/1799.png" },
     "Club Deportivo Popular Junior FC": { name: "Junior", flag: "", code: "JUN", crest: "https://crests.football-data.org/1799.png" },
 
+    // BRASILEIROS (SINCRONIZADOS COM TEAMS_BR.JS)
     "Palmeiras": { name: "Palmeiras", flag: "", code: "PAL", crest: "https://crests.football-data.org/1769.png" },
     "SE Palmeiras": { name: "Palmeiras", flag: "", code: "PAL", crest: "https://crests.football-data.org/1769.png" },
     "Flamengo": { name: "Flamengo", flag: "", code: "FLA", crest: "https://crests.football-data.org/1783.png" },
